a1d9aa4be6ab0c2292abebf3478d6ce65b56223e
Roohollah81 <m.roohollah.v@gmail.com>
2021-12-14 21:56:13 +0330

    اضافه كردن متن جديد به فايل متني قبل

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,5 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'My name is Roohollah.')
+//fs.writeFileSync('notes.txt', 'My name is Roohollah.')
+
+fs.appendFileSync('notes.txt', ' man daneshjo hastam')
