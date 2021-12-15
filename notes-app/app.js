3fe09a0deb15a9fa8a7d8d1dc1970b4e0bbf0b3b
Roohollah81 <m.roohollah.v@gmail.com>
2021-12-15 21:30:49 +0330

    ارتباط بين دوفايل

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
+const getNotes = require('./notes.js')
 
-//fs.writeFileSync('notes.txt', 'My name is Roohollah.')
+const msg = getNotes()
 
-fs.appendFileSync('notes.txt', ' man daneshjo hastam')
+console.log(msg)
