45bab17a57b3ff446a16dd2f9d71f488b461367e
Roohollah81 <m.roohollah.v@gmail.com>
2021-12-16 20:06:59 +0330

    نصب و استفاده از yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,40 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const command = process.argv[2]
 
-console.log(process.argv)
+yargs.version('1.1.0')
 
-if (command === 'add'){
-    console.log('Adding note!')  
-} else if (command === 'remove'){
-    console.log('Removing note!')
-}
+yargs.command ({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note!')
+    }
+})
+
+yargs.command ({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing the note')
+    }
+})
+
+yargs.command ({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function() {
+        console.log('Listing out all note')
+    }
+})
+
+yargs.command ({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading a note')
+    }
+})
+
+console.log(yargs.argv)
