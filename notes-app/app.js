1342fce0c04c796aaa2a6fbc9e38f60d47788edb
Roohollah81 <m.roohollah.v@gmail.com>
2021-12-24 10:43:57 +0330

    add option to yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,14 +2,29 @@ const chalk = require('chalk')
 const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-
+//Customize yarg version
 yargs.version('1.1.0')
 
+
+//Create add command
 yargs.command ({
     command: 'add',
     describe: 'Add a new note',
-    handler: function() {
-        console.log('Adding a new note!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
     }
 })
 
@@ -37,4 +52,4 @@ yargs.command ({
     }
 })
 
-console.log(yargs.argv)
+yargs.parse ()
