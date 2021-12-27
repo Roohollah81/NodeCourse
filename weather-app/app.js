da3a1f12a830b2bb94a45153c1ad25089b07dc7a
Roohollah81 <m.roohollah.v@gmail.com>
2021-12-27 20:41:41 +0330

    برنامه ي اعلام آب و هوا . خطا داشت!

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,7 @@
 const request = require('postman-request')
                 
-const url = 'http://api.weatherstack.com/current?access_key=2dc00b75f0bdc5f3b5f858adc8da7d8e&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=2dc00b75f0bdc5f3b5f858adc8da7d8e&query=37.8267,-122.4233&units=f'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.current.weather_description[0] + ". It is currently " + response.body.current.temprature + " degress out. It feels like " + response.body.current.feelslike + " degress out.")
 })
