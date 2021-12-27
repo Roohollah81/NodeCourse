ef175c12304b5c0c4c6881ccd9b4ad904b106b60
Roohollah81 <m.roohollah.v@gmail.com>
2021-12-27 12:45:20 +0330

    ساخت http

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,8 @@
-console.log('Strting')
+const request = require('postman-request')
+                
+const url = 'http://api.weatherstack.com/current?access_key=2dc00b75f0bdc5f3b5f858adc8da7d8e&query=37.8267,-122.4233'
 
-setTimeout(() => {
-    console.log('2 Second Timer')
-}, 2000)
-
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0)
-console.log('Stopting')
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
