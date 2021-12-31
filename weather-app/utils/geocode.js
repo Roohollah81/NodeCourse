06072edb6ee25a8647f07a370fa1173b7c341af9
Roohollah81 <m.roohollah.v@gmail.com>
2021-12-31 10:37:43 +0330

    خلاصه نويسي در چند فايل

--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -4,16 +4,16 @@ const request = require('postman-request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoicm9vaG9sbGFodiIsImEiOiJja3hveTIya2IyYTY3MnJwZWZyYXNuNmUzIn0.qWmTjS5g5rGP-jkEO-a69g&limit=1'
 
-    request({ url: url, json:true}, (error, Response) => {
+    request({ url, json:true}, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (Response.body.features.length === 0) {
+        } else if (body.features.length === 0) {
             callback('Unable to find location. try another search.', undefined)
         } else {
             callback(undefined, {
-                latitude: Response.body.features[0].center[0],
-                longtude: Response.body.features[0].center[1],
-                location: Response.body.features[0].place_name
+                latitude: body.features[0].center[0],
+                longtude: body.features[0].center[1],
+                location: body.features[0].place_name
             })
         }
     })
