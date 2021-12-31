69d0837871b7d4c073771fdf751ba6b13c896c94
Roohollah81 <m.roohollah.v@gmail.com>
2021-12-31 09:52:06 +0330

    دادن اسم مكان و گرفتن موقعيت مكاني و آب و هوا

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,37 +1,23 @@
-const request = require('postman-request')
 const geocode = require('./utils/geocode') 
 const forecast = require('./utils/forecast')
 
-// const url = 'http://api.weatherstack.com/current?access_key=2dc00b75f0bdc5f3b5f858adc8da7d8e&query=37.8267,-122.4233&units=f'
+const address = process.argv[2]
 
-// request({ url: url, json: true }, (error, response) => {
-//     console.log(response.body.current.weather_description[0] + ". It is currently " + response.body.current.temprature + " degress out. It feels like " + response.body.current.feelslike + " degress out.")
-// })
-
-
-
-// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1Ijoicm9vaG9sbGFodiIsImEiOiJja3hveTIya2IyYTY3MnJwZWZyYXNuNmUzIn0.qWmTjS5g5rGP-jkEO-a69g&limit=1'
-
-// request({ url: geocodeURL, json: true}, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather servise!')
-//     } else if (response.body.features.length === 0) {
-//         console.log('Unable to find location. Try another search.')
-//     } else {
-//         const latitude = response.body.features[0].center[1]
-//         const longitude = response.body.features[0].center[0]
-//         console.log(latitude, longitude)
-//     }
-// })
-
-
-
-// geocode('Boston', (error, data) => {
-//     console.log('Error', error)
-//     console.log('Data', data)
-// })
-
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+    
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+    
+            console.log(data.location)
+            console.log(forecastData) 
+        })
+    })   
+}
