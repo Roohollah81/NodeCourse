8bd9e84a3226599c7c837a3dcb92beb842076619
Roohollah81 <m.roohollah.v@gmail.com>
2022-01-10 09:51:22 +0330

    فصل 8 ويديو سوم خطا داشت

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const { registerPartial } = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -10,14 +11,14 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-// Setup static directory to srver
+// Setup static directory to ser
 app.use(express.static(publicDirectoryPath))
+
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
@@ -43,16 +44,29 @@ app.get('/help', (req, res) => {
 app.get('/weather', (req, res) => { 
     if (!req.query.address) {
         return res.send({
-            error: 'You must provide an address'
+            error: 'You must provide an address!'
         })
     }
-    res.send({
-        forcast: 'It is snowing',
-        location: 'Philadelphia',
-        address: req.query.address
+
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
 })
- 
+
 app.get('/products', (req, res) => {
     if (!req.query.search) {
         return res.send({
@@ -68,9 +82,9 @@ app.get('/products', (req, res) => {
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
-    title: '404',
-    name: 'Andrew Mead',
-    errorMessage: 'Help article not found'
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
     })
 })
 
@@ -81,7 +95,6 @@ app.get('*', (req, res) => {
         errorMessage: 'Page not found.'
     })
 })
-
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
