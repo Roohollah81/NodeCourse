a5731afb44814d66c714a4cacba53e279e6011d7
Roohollah81 <m.roohollah.v@gmail.com>
2022-01-11 09:01:37 +0330

    fetch weather (بازم ارور)

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -20,12 +20,12 @@ const product = {
     rating: 4.2
 }
 
-const {label: productLabel, stock, rating = 5} = product
-console.log(productLabel)
-console.log(stock)
-console.log(rating)
+// const {label: productLabel, stock, rating = 5} = product
+// console.log(productLabel)
+// console.log(stock)
+// console.log(rating)
 
-const transaction = (type, { label, stock }) => {
+const transaction = (type, { label, stock = 0 } = {}) => {
     console.log(type, label, stock)
 }
 
